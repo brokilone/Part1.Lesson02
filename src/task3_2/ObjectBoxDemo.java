1e71740272acf974ef2dabefd72b83967b04e0ea
Kseniya Ushakova <brokilone@yandex.ru>
2020-05-13 00:43:00 +0500

    Lesson03

---
+++ src/task3_2/ObjectBoxDemo.java
@@ -0,0 +1,24 @@
+package task3_2;
+
+/**
+ * ObjectBoxDemo
+ * класс демонструрует работу класса ObjectBox
+ * created by Ksenya_Ushakova at 29.04.2020
+ */
+public class ObjectBoxDemo {
+    /**
+     * Главный метод для демонстрации
+     * @param args
+     */
+    public static void main(String[] args) {
+        Object[] objects = {"one", "two", "three"};
+        ObjectBox box = new ObjectBox(objects);
+
+        ObjectBox box2 = box.addObject("four");
+
+        box2 = box2.deleteObject("one");
+
+        System.out.println(box.dump());
+        System.out.println(box2.dump());
+    }
+}
